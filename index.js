832f17a357d3186eab6259a2158767158f4972e8
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-05-01 20:13:12 +0300

    "Last commit"

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const require('datejs');
 //Create a function named combineUsers that will contain an indeterminite amount of arrays
 function combineUsers(...args) {
   const combinedObjects = {users: []}; //Variable with key of users
-  for (const userArray of args) {
-    combinedObjects.users = [...combinedObjects.users, ...userArray]; // loop throough args to isolate each array
+  for (const userArray of args) { // loop throough args to isolate each array
+    combinedObjects.users = [...combinedObjects.users, ...userArray]; 
   }
 
-  combinedObjects.merge_date = Date.today().toString('M/d/yyyy');
+  combinedObjects.merge_date = Date.today().toString('M/d/yyyy'); //Fetch dates
   return combinedObjects;
   
 }
