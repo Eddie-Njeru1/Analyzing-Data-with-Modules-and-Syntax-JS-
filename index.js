7a466a8bd42d4e34fe73fa7d97dc69c3c2da38ef
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-05-01 20:11:02 +0300

    "Final Commit"

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+//Readline require to link datejs in json for the date
+const require('datejs');
 
+//Create a function named combineUsers that will contain an indeterminite amount of arrays
+function combineUsers(...args) {
+  const combinedObjects = {users: []}; //Variable with key of users
+  for (const userArray of args) {
+    combinedObjects.users = [...combinedObjects.users, ...userArray]; // loop throough args to isolate each array
+  }
 
+  combinedObjects.merge_date = Date.today().toString('M/d/yyyy');
+  return combinedObjects;
+  
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
